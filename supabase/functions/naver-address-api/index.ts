8db3eb70ae2fcf9e4f75d940df906cd2258ae56b
orlein <pcj9024@gmail.com>
2025-02-03 22:55:52 +0900

    update supabase functions

--- supabase/functions/naver-address-api/index.ts
+++ supabase/functions/naver-address-api/index.ts
@@ -5,6 +5,7 @@
 // Setup type definitions for built-in Supabase Runtime APIs
 import 'jsr:@supabase/functions-js/edge-runtime.d.ts';
 import { Hono } from 'jsr:@hono/hono';
+import { cors } from 'jsr:@hono/hono/cors';
 
 const functionName = 'naver-address-api';
 const app = new Hono().basePath(`/${functionName}`);
@@ -14,6 +15,7 @@ const fetchToNaver = async (query: string) => {
   url.searchParams.set('query', query);
   const response = await fetch(url, {
     method: 'GET',
+
     headers: {
       'Content-Type': 'application/json',
       'X-Naver-Client-Id': Deno.env.get('NAVER_DEVELOPERS_CLIENT_ID')!,
@@ -24,10 +26,28 @@ const fetchToNaver = async (query: string) => {
   return await response.text();
 };
 
+app.use(
+  '/*',
+  cors({
+    origin: [
+      'http://localhost:3000',
+      'https://course-builder-liard.vercel.app/',
+      'https://course-builder-orcin-sigma.vercel.app/',
+      'https://oz-adv-course-builder.vercel.app/',
+    ],
+    allowMethods: ['GET', 'OPTIONS'],
+  }),
+);
+
+app.options('/', () => {
+  return new Response('ok');
+});
+
 app.get('/', async (c) => {
   try {
     const { query } = c.req.query();
-    const data = await fetchToNaver(query);
+    const decoded = decodeURIComponent(query);
+    const data = await fetchToNaver(decoded);
     return new Response(data);
   } catch (error) {
     return new Response(JSON.stringify(error), { status: 500 });
